8e5b620b09f81313d5335c76dbc2c4ac1656e4df
Devy <izabela.marcinkowska@outlook.com>
2024-01-13 22:25:14 +0100

    style: Change cards till shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -6,46 +6,21 @@ import ActionCard from "@/components/ActionCard";
 export default function Home() {
   return (
     <div className=" flex flex-col gap-2 mx-2 my-4">
-      <div className="bg-white shadow-xl flex flex-col">
-        <figure>
-          <Image src={whatitis} alt="Album" className="w-72" />
-        </figure>
-        <div className="">
-          <h2 className="">What is It?</h2>
-          <p>
-            Our platform is an innovative solution for individuals with
+      <ActionCard
+        img={whatitis}
+        title="What is it?"
+        description="Our platform is an innovative solution for individuals with
             Irritable Bowel Syndrome (IBS). It acts as a digital diary and
             analysis tool, helping users track and manage their IBS symptoms
             with ease. Using advanced AI, we simplify the tracking of food
             intake, bowel movements, and health indicators, making IBS
-            management more accessible.
-          </p>
-        </div>
-      </div>
-
-      <div className="bg-white shadow-xl">
-        <figure>
-          <Image src={howitworks} alt="Album" className="w-72" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">How it works?</h2>
-          <p>
-            * Daily Tracking: Users log their food, bowel types, alcohol,
-            medication, pain, stress levels, and feelings daily.{" "}
-          </p>
-          <p>
-            * Data Analysis: With a click, the AI analyzes these inputs,
-            correlating diet and stress with IBS symptoms.
-          </p>{" "}
-          <p>
-            * Personalized Insights: Users receive customized feedback,
-            identifying potential dietary triggers and offering IBS-friendly
-            recommendations.
-          </p>
-        </div>
-      </div>
-
-      <ActionCard />
+            management more accessible."
+      />
+      <ActionCard
+        img={howitworks}
+        title="How it works?"
+        description="You log your food, bowel types, alcohol, medication, pain, stress levels, and feelings daily. With a click, the AI analyzes your inputs, correlating your diet and stress with IBS symptoms. You receive personalized insights, identifying potential dietary triggers and offering IBS-friendly recommendations."
+      />
     </div>
   );
 }
--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 import {
   Card,
   CardContent,
@@ -17,42 +19,26 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { StaticImageData } from "next/image";
 
-const ActionCard = () => {
+type ActionCardProps = {
+  img: StaticImageData;
+  title: string;
+  description: string;
+};
+
+const ActionCard = ({ img, title, description }: ActionCardProps) => {
   return (
     <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
       <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
+        {/* <div>
+          <Image src={img} alt="Album" className="w-72" />
+        </div> */}
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
     </Card>
   );
 };
