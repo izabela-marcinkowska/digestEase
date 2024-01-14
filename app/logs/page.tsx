8c0dfe68cb1acd57f2a757026dfffe14aa33b440
Devy <izabela.marcinkowska@outlook.com>
2024-01-14 14:29:28 +0100

    feat: Add supabase

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -1,7 +1,32 @@
 import { PlusCircle } from "lucide-react";
 import Link from "next/link";
 
-const Logs = () => {
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export type Log = {
+  id: string;
+  date: string;
+  foodInput: string[];
+  alcohol: boolean;
+  bowelMovements: string;
+  stress: number;
+  pain: boolean;
+  nausea: boolean;
+};
+
+const Logs = async () => {
+  const getLogs = async () => {
+    const { data, error } = await supabase
+      .from("Logs")
+      .select()
+      .order("date", { ascending: false });
+    if (error) throw error;
+    return data;
+  };
+  const logs = await getLogs();
   return (
     <div>
       <div className="flex justify-between m-10">
@@ -9,6 +34,11 @@ const Logs = () => {
         <Link href={"/add-log"}>
           <PlusCircle size={34} />
         </Link>
+      </div>
+      <div className="ml-2 mr-2">
+        {logs.map((log: Log) => (
+          <Log key={log.id} log={log} />
+        ))}
       </div>
     </div>
   );
