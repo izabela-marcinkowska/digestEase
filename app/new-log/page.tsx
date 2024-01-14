8163eca852221a0f9ba30c851c6647b50ca8c09a
Devy <izabela.marcinkowska@outlook.com>
2024-01-14 23:13:01 +0100

    feat: Add form component

--- app/new-log/page.tsx
+++ app/new-log/page.tsx
@@ -1,7 +1,8 @@
+import LogForm from "../../components/LogForm";
 const NewLog = () => {
   return (
-    <div className="flex justify-between mx-6 my-10">
-      <h1 className="text-2xl">Add your new log:</h1>
+    <div className="justify-between">
+      <LogForm />
     </div>
   );
 };
