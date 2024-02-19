5167ba21d287ef5f6cb4482e38c218911be2f7cf
Devy <izabela.marcinkowska@outlook.com>
2024-02-19 22:15:07 +0100

    feat: Move Card text outside of the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
-import Image from "next/image";
-import whatitis from "../assets/whatitis.png";
-import howitworks from "../assets/howitworks.png";
-import ActionCard from "@/components/ActionCard";
+import Image from 'next/image';
+import whatitis from '../assets/whatitis.png';
+import howitworks from '../assets/howitworks.png';
+import { howItWorksDescription, whatIsItDescription } from '@/content';
+import ActionCard from '@/components/ActionCard';
 
 export default function Home() {
   return (
-    <div className=" flex flex-col gap-2 mx-2 my-4">
-      <ActionCard
-        img={whatitis}
-        title="What is it?"
-        description="Our platform is an innovative solution for individuals with
-            Irritable Bowel Syndrome (IBS). It acts as a digital diary and
-            analysis tool, helping users track and manage their IBS symptoms
-            with ease. Using advanced AI, we simplify the tracking of food
-            intake, bowel movements, and health indicators, making IBS
-            management more accessible."
-      />
-      <ActionCard
-        img={howitworks}
-        title="How it works?"
-        description="You log your food, bowel types, alcohol, medication, pain, stress levels, and feelings daily. With a click, the AI analyzes your inputs, correlating your diet and stress with IBS symptoms. You receive personalized insights, identifying potential dietary triggers and offering IBS-friendly recommendations."
-      />
+    <div>
+      <h1 className="text-gray-700 text-3xl font-logo">DigestEase</h1>
     </div>
+
+    // <div className=" flex flex-col gap-2 mx-2 my-4 bg-green-50">
+    //   <ActionCard
+    //     img={whatitis}
+    //     title="What is it?"
+    //     description={whatIsItDescription}
+    //   />
+    //   <ActionCard
+    //     img={howitworks}
+    //     title="How it works?"
+    //     description={howItWorksDescription}
+    //   />
+    // </div>
   );
 }
---
+++ content/index.ts
@@ -0,0 +1,5 @@
+export const whatIsItDescription =
+  'Our platform is an innovative solution for individuals with Irritable Bowel Syndrome (IBS). It acts as a digital diary and analysis tool, helping users track and manage their IBS symptoms with ease. Using advanced AI, we simplify the tracking of food intake, bowel movements, and health indicators, making IBS management more accessible.';
+
+export const howItWorksDescription =
+  'You log your food, bowel types, alcohol, medication, pain, stress levels, and feelings daily. With a click, the AI analyzes your inputs, correlating your diet and stress with IBS symptoms. You receive personalized insights, identifying potential dietary triggers and offering IBS-friendly recommendations.';
