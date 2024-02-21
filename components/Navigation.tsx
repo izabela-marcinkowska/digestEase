7601f80d79390f5b09a8514d49924ff3b7d4d350
Devy <izabela.marcinkowska@outlook.com>
2024-02-21 09:55:58 +0100

    feat: Add shadow and form to the nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,24 +1,25 @@
-import Link from "next/link";
-import logo from "../assets/logo.png";
-import Image from "next/image";
+'use client';
+import Link from 'next/link';
+import logo from '../assets/logosvg.svg';
+import Image from 'next/image';
 
 const Navigation = () => {
   return (
-    <nav className="flex">
+    <nav className="flex shadow-[0_0_5px_1px_rgba(0,0,0,0.1)] md:w-5/6 mx-auto md:mt-4">
       <div className="flex-1">
-        <Link href={"/"}>
+        <Link href={'/'}>
           <Image src={logo} className="my-3 mx-5" height={50} alt="logo" />
         </Link>
       </div>
       <div className="">
         <ul className=" flex gap-2 m-5">
           <li>
-            <Link href={"/logs"} className="text-xl">
+            <Link href={'/logs'} className="text-xl">
               Logs
             </Link>
           </li>
           <li>
-            <Link href={"/rapports"} className="text-xl">
+            <Link href={'/rapports'} className="text-xl">
               Rapports
             </Link>
           </li>
