70340e6a263b9bee6e13c0c8559ce260c61479f4
Devy <izabela.marcinkowska@outlook.com>
2024-02-22 20:32:35 +0100

    feat: Add X button to navigation

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -1,13 +1,24 @@
 'use client';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import OpenMenu from './OpenMenu';
 import { usePathname } from 'next/navigation';
 
 type Props = {
   visible: boolean;
 };
+
+export const NAV_LINKS = [
+  {
+    text: 'Logs',
+    href: '/logs',
+  },
+  {
+    text: 'Rapports',
+    href: '/.rapports',
+  },
+];
 
 const Hamburger = ({ visible }: Props) => {
   const [isOpen, setOpen] = useState(false);
@@ -34,23 +45,28 @@ const Hamburger = ({ visible }: Props) => {
   return (
     <>
       <div>
-        <Menu
-          className="md:hidden m-4"
-          size={46}
-          onClick={handleHamburgerClick}
-        />
+        {!isOpen ? (
+          <Menu
+            className="md:hidden m-4"
+            size={46}
+            onClick={handleHamburgerClick}
+          />
+        ) : (
+          <X
+            className="md:hidden m-4"
+            size={46}
+            onClick={handleHamburgerClick}
+          />
+        )}
         <OpenMenu visible={isOpen} />
         <ul className="md:flex gap-2 m-5 hidden">
-          <li>
-            <Link href={'/logs'} className="text-xl">
-              Logs
-            </Link>
-          </li>
-          <li>
-            <Link href={'/rapports'} className="text-xl">
-              Rapports
-            </Link>
-          </li>
+          {NAV_LINKS.map((link, index) => (
+            <li key={index}>
+              <Link href={link.href} className="text-xl">
+                {link.text}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </>
