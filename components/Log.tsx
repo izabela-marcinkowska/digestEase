fc7ace04a1e973b04ffcc08abaaa84385d873a0e
Devy <izabela.marcinkowska@outlook.com>
2024-02-26 21:51:56 +0100

    feat: Show details of log more separate

--- components/Log.tsx
+++ components/Log.tsx
@@ -18,14 +18,20 @@ const Log = ({ log }: LogProps) => {
       >
         <AccordionItem value="item-1">
           <AccordionTrigger>{log.date}</AccordionTrigger>
-          <AccordionContent>
-            <div>Date: {log.date}</div>
-            <div>Food Input: {log.foodInput.join(', ')}</div>
-            <div>Alcohol: {log.alcohol ? 'Yes' : 'No'}</div>
-            <div>Bowel Movements: {log.bowelMovements}</div>
-            <div>Stress Level: {log.stress}</div>
-            <div>Pain: {log.pain ? 'Yes' : 'No'}</div>
-            <div>Nausea: {log.nausea ? 'Yes' : 'No'}</div>
+          <AccordionContent className="flex justify-around">
+            <div>
+              <p>Food:</p>
+              {log.foodInput.map((food, index) => (
+                <p key={index}>{food}</p>
+              ))}
+            </div>
+            <div>
+              <div>Alcohol: {log.alcohol ? 'Yes' : 'No'}</div>
+              <div>Stress Level: {log.stress}</div>
+              <div>Bowel Movements: {log.bowelMovements}</div>
+              <div>Pain: {log.pain ? 'Yes' : 'No'}</div>
+              <div>Nausea: {log.nausea ? 'Yes' : 'No'}</div>
+            </div>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
