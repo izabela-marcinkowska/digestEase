5645b68576442d486e33cffd5466657a557e603f
Devy <izabela.marcinkowska@outlook.com>
2024-02-27 20:19:56 +0100

    feat: Remove unnacesary margin on log page

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -2,7 +2,7 @@ import LogBox from '@/components/LogBox';
 
 const Logs = async () => {
   return (
-    <div className="md:w-5/6 w-11/12 mx-auto">
+    <div className="md:w-5/6 mx-auto">
       <LogBox />
     </div>
   );
