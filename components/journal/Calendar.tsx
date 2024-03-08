2cb737f8b9d5af6bdc7b0200c5a869f252e16bd8
Devy <izabela.marcinkowska@outlook.com>
2024-03-08 17:48:04 +0100

    feat: Create function returning monday and sunday DIG-125

--- components/journal/Calendar.tsx
+++ components/journal/Calendar.tsx
@@ -1,9 +1,16 @@
+'use client';
+import { startOfWeek, endOfWeek } from 'date-fns';
+
+const today = new Date();
+function getWeekStartAndEnd(date: Date) {
+  const monday = startOfWeek(date, { weekStartsOn: 1 }); // Monday
+  const sunday = endOfWeek(date, { weekStartsOn: 1 }); // Sunday
+  return [monday, sunday];
+}
+
 const Calendar = () => {
-  return (
-    <>
-      <p>Calendar</p>
-    </>
-  );
+  const result = getWeekStartAndEnd(today);
+  console.log(result[0]);
 };
 
 export default Calendar;
