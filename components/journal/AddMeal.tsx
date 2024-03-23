55e74136db869c105141d7c4ca3c552c94357105
Devy <izabela.marcinkowska@outlook.com>
2024-03-23 22:17:11 +0100

    feat: Create AddMeal component

---
+++ components/journal/AddMeal.tsx
@@ -0,0 +1,9 @@
+const AddMeal = () => {
+  return (
+    <>
+      <h1>This is Add box</h1>
+    </>
+  );
+};
+
+export default AddMeal;
--- components/journal/FoodBox.tsx
+++ components/journal/FoodBox.tsx
@@ -1,10 +1,12 @@
 import { FoodType, foodBoxProp } from '@/content/types';
 import Meal from './Meal';
+import AddMeal from './AddMeal';
 
 const FoodBox = ({ meals, id }: foodBoxProp) => {
   return (
     <>
       <div className="flex gap-2 flex-col md:flex-row w-11/12 md:w-full md:ml-3 mx-auto">
+        <AddMeal />
         {meals.map((meal, index) => (
           <Meal key={index} id={id} food={meal.food} type={meal.type} />
         ))}
