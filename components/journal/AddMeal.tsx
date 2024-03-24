8d22c18d63171339997d0ac408556d9710484d6f
Devy <izabela.marcinkowska@outlook.com>
2024-03-24 15:52:48 +0100

    feat: Function takes correct id and creates log

--- components/journal/AddMeal.tsx
+++ components/journal/AddMeal.tsx
@@ -1,16 +1,35 @@
 import { useState } from 'react';
 import supabaseClient from '@/lib/supabase/client';
 import { toast } from 'sonner';
+import { useDateStore } from '@/app/dateStore';
 import { AddMealProp } from '@/content/types';
 
 const AddMeal = ({ id }: AddMealProp) => {
   const [newMeal, setNewMeal] = useState({});
+  const [createdIdLog, setCreatedIdLog] = useState('');
+  const pickedDay = useDateStore((state) => state.chosenDay);
 
-  const addNewMeal = async (food: string[], type: string, log: string) => {
+  const checkId = async (insertedId: string) => {
+    if (!insertedId) {
+      const { data, error } = await supabaseClient
+        .from('logs')
+        .insert({ date: pickedDay })
+        .select(id);
+      setCreatedIdLog(id);
+      console.log('this is createdIdLog', createdIdLog);
+      console.log(error);
+      console.log('now is day created', data);
+    } else {
+      setCreatedIdLog(insertedId);
+    }
+  };
+
+  const addNewMeal = async (food: string[], type: string, id: string) => {
     try {
+      checkId(id);
       const { data, error } = await supabaseClient
         .from('meals')
-        .insert({ food, type, log });
+        .insert({ food, type, id: createdIdLog });
 
       if (error) {
         console.error('Error from Supabase when adding new meal:', error);
