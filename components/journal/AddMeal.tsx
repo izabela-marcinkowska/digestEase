9c70527e110d416439a05b0b55c9f4f9192deed9
Devy <izabela.marcinkowska@outlook.com>
2024-03-26 13:55:03 +0100

    chore: Clean up code

--- components/journal/AddMeal.tsx
+++ components/journal/AddMeal.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import supabaseClient from '@/lib/supabase/client';
 import { toast } from 'sonner';
 import { useDateStore } from '@/app/dateStore';
-import { AddMealProp, MealProp, SingleLog } from '@/content/types';
+import { AddMealProp } from '@/content/types';
 
 const AddMeal = ({ id }: AddMealProp) => {
-  const [newMeal, setNewMeal] = useState({});
   const pickedDay = useDateStore((state) => state.chosenDay);
 
   const checkId = async (insertedId: string): Promise<string> => {
@@ -14,27 +12,19 @@ const AddMeal = ({ id }: AddMealProp) => {
         .from('logs')
         .insert({ date: pickedDay })
         .select()
-        .single(); // Ensures a single record is expected in response
-      console.log(pickedDay, 'this is the date i picked');
+        .single();
       if (error || !data) {
-        console.error('Error creating or retrieving log:', error);
         throw new Error('Failed to create or retrieve log');
       }
-
-      console.log('This is id', data.id);
-      console.log('now is day created', data);
-      console.log('the date inside data', data.date);
-      return data.id; // TypeScript should now recognize `id` on `data`
+      return data.id;
     } else {
-      console.log('the log has been already created', insertedId);
       return insertedId;
     }
   };
 
   const addNewMeal = async (food: string[], type: string, id: string) => {
     try {
-      const logId = await checkId(id); // Await the ID from checkId
-      console.log('is the log correct now?', logId); // Use the directly returned ID
+      const logId = await checkId(id);
       const { data, error } = await supabaseClient
         .from('meals')
         .insert({ food, type, log: logId });
@@ -44,8 +34,6 @@ const AddMeal = ({ id }: AddMealProp) => {
         toast.error('Failed to create new meal due to a server error.');
         return null;
       }
-
-      console.log(data);
       return data;
     } catch (error) {
       console.error('Unexpected error when adding new meal:', error);
@@ -60,7 +48,6 @@ const AddMeal = ({ id }: AddMealProp) => {
 
   return (
     <>
-      {pickedDay}
       <h1 onClick={handleOnClick}>This is Add box</h1>
     </>
   );
