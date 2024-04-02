4cbec6ce770d694b2dbfd0e13f3fb16d6586d5c0
Devy <izabela.marcinkowska@outlook.com>
2024-04-02 22:15:19 +0200

    feat: Add meal to database and put dummydata

--- components/journal/AddMeal.tsx
+++ components/journal/AddMeal.tsx
@@ -13,10 +13,14 @@ const AddMeal = ({ journalId }: AddMealProp) => {
   const addMeal = useJournalStore((state) => state.addMeal);
   const setLog = useJournalStore((state) => state.setCurrentLog);
 
-  const addNewMeal = async () => {
+  const addNewMeal = async (type: string, food: string[]) => {
     setLoading(true);
     if (!journalId) {
-      const { data, error } = await supabaseClient.from('logs').insert({ date: pickedDay }).select().single();
+      const { data, error } = await supabaseClient
+        .from('logs')
+        .insert({ date: pickedDay })
+        .select()
+        .single();
       if (error) {
         console.error('Failed to create new log.');
         toast.error('Failed to create new log.');
@@ -25,13 +29,29 @@ const AddMeal = ({ journalId }: AddMealProp) => {
 
       if (data) {
         setLog(data);
-        addMeal({ id: uuid(), type: '', food: [], log: data.id }, true);
+        const newMealId = uuid();
+        addMeal({ id: newMealId, type, food, log: data.id }, true);
+        const { error } = await supabaseClient
+          .from('meals')
+          .insert({ id: newMealId, type, food, log: data.id });
+        if (error) {
+          console.log('failed to add meal to the database');
+          toast.error('Failed to add new meal.');
+        }
         setLoading(false);
       }
     }
 
     if (journalId) {
-      addMeal({ id: uuid(), type: '', food: [], log: journalId }, true);
+      const newMealId = uuid();
+      addMeal({ id: newMealId, type, food, log: journalId }, true);
+      const { error } = await supabaseClient
+        .from('meals')
+        .insert({ id: newMealId, type, food, log: journalId });
+      if (error) {
+        console.log('failed to add meal to the database');
+        toast.error('Failed to add new meal.');
+      }
       setLoading(false);
     }
   };
@@ -39,7 +59,7 @@ const AddMeal = ({ journalId }: AddMealProp) => {
   return (
     <>
       <button
-        onClick={addNewMeal}
+        onClick={() => addNewMeal('breakfast', ['cola', 'cheese'])}
         className="border rounded-lg shadow-sm w-24 h-24 flex justify-center items-center bg-lightGreen cursor-pointer hover:shadow-inner disabled:opacity-50"
         disabled={loading}
       >
