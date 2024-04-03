7b0bed55bde0acf5b95dae9c7c6b5aaeacd093d6
Devy <izabela.marcinkowska@outlook.com>
2024-04-03 13:21:15 +0200

    feat: Create form example for meal form

--- components/journal/MealForm.tsx
+++ components/journal/MealForm.tsx
@@ -1,7 +1,31 @@
+import { FormInputs } from '@/content/types';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
 const MealForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
+  console.log(watch('type')); // watch input value by passing the name of it
+
   return (
     <>
       <h1>This will be a Form here</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register('type')} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register('food', { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.food && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </>
   );
 };
