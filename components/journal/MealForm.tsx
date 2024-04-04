c74948014b28786827f090b3ef007b2b00e15b78
Devy <izabela.marcinkowska@outlook.com>
2024-04-04 11:14:04 +0200

    feat:Create and show meal form  DIG-70 DIG-51 DIG-52

--- components/journal/MealForm.tsx
+++ components/journal/MealForm.tsx
@@ -1,7 +1,10 @@
 import { FormInputs } from '@/content/types';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 const MealForm = () => {
+  const [foodList, setFoodList] = useState<string[]>([]);
+  const [currentFood, setCurrentFood] = useState('');
   const {
     register,
     handleSubmit,
@@ -11,6 +14,12 @@ const MealForm = () => {
   const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
 
   console.log(watch('type')); // watch input value by passing the name of it
+
+  const handleAddFoodItem = () => {
+    if (!currentFood) return; // Don't add if the input is empty
+    setFoodList((prevFoodList) => [...prevFoodList, currentFood]);
+    setCurrentFood(''); // Clear the input field after adding the food item
+  };
 
   return (
     <>
@@ -25,8 +34,27 @@ const MealForm = () => {
           <option value="snacks">Snacks</option>
         </select>
         {errors.food && <span>This field is required</span>}
-        <input {...register('food', { required: true })} />
-        {errors.food && <span>This field is required</span>}
+        <div>
+          <span>What did you eat today?</span>
+          <input
+            type="text"
+            value={currentFood}
+            onChange={(e) => setCurrentFood(e.target.value)} // Update the currentFood state with the input's value
+          />
+          <button
+            type="button"
+            onClick={handleAddFoodItem}
+            className="btn btn-primary"
+          >
+            Add
+          </button>
+          {errors.food && <span>This field is required</span>}
+          <ul>
+            {foodList.map((food, index) => (
+              <li key={index}>{food}</li> // Display each food item as a list item
+            ))}
+          </ul>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </>
