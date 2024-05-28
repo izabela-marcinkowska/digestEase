efba475946d7b1a216988e95326b8f010a77f85e
Johnie Hjelm <johnie@hjelm.im>
2024-05-28 19:41:53 +0200

    Remove loading state from AddMeal component

--- components/journal/AddMeal.tsx
+++ components/journal/AddMeal.tsx
@@ -5,7 +5,6 @@ import { Plus } from 'lucide-react';
 import MealForm from './MealForm';
 
 const AddMeal = ({ journalId }: AddMealProp) => {
-  const [loading, setLoading] = useState(false);
   const formStatus = useDateStore((state) => state.formStatus);
   const toggleFormStatus = useDateStore((state) => state.toggleFormStatus);
 
@@ -19,7 +18,6 @@ const AddMeal = ({ journalId }: AddMealProp) => {
         <button
           onClick={handleFormStatus}
           className="border rounded-lg shadow-sm size-[85px] flex justify-center items-center bg-lightGreen cursor-pointer hover:shadow-inner disabled:opacity-50"
-          disabled={loading}
         >
           <Plus size={45} color="darkGreen" />
         </button>
