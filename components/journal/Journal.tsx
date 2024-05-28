758d13996a43d5d57fbcd384690894df26a33eaf
Johnie Hjelm <johnie@hjelm.im>
2024-05-28 20:23:43 +0200

    Refactor Journal component to remove unused code

--- components/journal/Journal.tsx
+++ components/journal/Journal.tsx
@@ -9,7 +9,6 @@ const Journal = () => {
   const pickedDay = useDateStore((state) => state.chosenDay);
   const chosenLog = useJournalStore((state) => state.log);
   const setChosenLog = useJournalStore((state) => state.setCurrentLog);
-  const toggleFormStatus = useDateStore((state) => state.toggleFormStatus);
 
   useEffect(() => {
     const getLog = async () => {
@@ -24,10 +23,7 @@ const Journal = () => {
 
   return (
     <div className="w-5/6 mx-auto mt-16">
-      <FoodBox
-        meals={chosenLog ? chosenLog.meals : null}
-        id={chosenLog ? chosenLog.id : ''}
-      />
+      <FoodBox meals={chosenLog ? chosenLog.meals : null} id={chosenLog ? chosenLog.id : ''} />
     </div>
   );
 };
