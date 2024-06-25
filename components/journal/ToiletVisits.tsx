aa08a91243549f73fe71ea8c5a69bd14d4b2a910
Devy <izabela.marcinkowska@outlook.com>
2024-06-25 15:52:30 +0200

    feat: Add carusel for toilet visit

--- components/journal/ToiletVisits.tsx
+++ components/journal/ToiletVisits.tsx
@@ -11,6 +11,14 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormLabel, FormField } from '@/components/ui/form';
 import supabaseClient from '@/lib/supabase/client';
 import { useState } from 'react';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { Card, CardContent } from '../ui/card';
 
 const toiletVisitSchema = z.object({
   id: z.number().optional(),
@@ -58,7 +66,30 @@ export function ToiletVisits({ id }: { id: string }) {
     <div className="flex flex-col gap-2">
       {showToiletForm ? (
         <div>
-          <Button onClick={() => handleAddToiletVisit(4)}>Visit</Button>
+          <Carousel
+            opts={{
+              align: 'start',
+            }}
+            className="w-full max-w-sm"
+          >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                  <div className="p-1">
+                    <Card onClick={() => handleAddToiletVisit(index + 1)}>
+                      <CardContent className="flex aspect-square items-center justify-center p-6">
+                        <span className="text-3xl font-semibold">
+                          {index + 1}
+                        </span>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
       ) : (
         <div>
